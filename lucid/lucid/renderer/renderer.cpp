2ee5abf86f30fee7795248673950c4620a4b7e40
Connor Smith <88218388+EdwinS7@users.noreply.github.com>
2023-04-03 20:23:39 -0400

    lol

--- lucid/lucid/renderer/renderer.cpp
+++ lucid/lucid/renderer/renderer.cpp
@@ -1,34 +1,27 @@
 #include "renderer.h"
 
-compiled_draw_data_t lucid_engine::renderer::get_draw_data() {
-	compiled_draw_data_t compiled;
-	compiled.vertices = draw_data;
-	
-	return compiled;
-}
-
-void lucid_engine::renderer::render_draw_data(compiled_draw_data_t draw_data) {
+void lucid_engine::renderer::render_draw_data() {
 	static int vertex_buffer_size{ 5000 }, index_buffer_size{ 10000 };
 
-	if (!vertex_buffer || vertex_buffer_size < draw_data.total_vertex_count) {
+	if (!vertex_buffer || vertex_buffer_size < compiled_draw_data.total_vertex_count) {
 		if (vertex_buffer) {
 			vertex_buffer->Release();
 			vertex_buffer = nullptr;
 		}
 
-		vertex_buffer_size = draw_data.total_vertex_count + 5000;
+		vertex_buffer_size = compiled_draw_data.total_vertex_count + 5000;
 
 		if (lucid_engine::graphics::get_instance().direct_3d_device->CreateVertexBuffer(vertex_buffer_size * sizeof(vertex_t), D3DUSAGE_DYNAMIC | D3DUSAGE_WRITEONLY, D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1, D3DPOOL_DEFAULT, &vertex_buffer, nullptr) < 0)
 			throw std::runtime_error{ "CreateVertexBuffer error" };
 	}
 
-	if (!index_buffer || index_buffer_size < draw_data.total_index_count) {
+	if (!index_buffer || index_buffer_size < compiled_draw_data.total_index_count) {
 		if (index_buffer) {
 			index_buffer->Release();
 			index_buffer = nullptr;
 		}
 
-		index_buffer_size = draw_data.total_index_count + 10000;
+		index_buffer_size = compiled_draw_data.total_index_count + 10000;
 
 		if (lucid_engine::graphics::get_instance().direct_3d_device->CreateIndexBuffer(index_buffer_size * sizeof(std::uint32_t), D3DUSAGE_DYNAMIC | D3DUSAGE_WRITEONLY, D3DFMT_INDEX32, D3DPOOL_DEFAULT, &index_buffer, nullptr) < 0)
 			throw std::runtime_error{ "CreateIndexBuffer error" };
@@ -46,9 +39,9 @@ void lucid_engine::renderer::render_draw_data(compiled_draw_data_t draw_data) {
 	vertex_t* vtx_dst{ };
 	std::uint32_t* idx_dst{ };
 
-	if (vertex_buffer->Lock(0, (UINT)(draw_data.total_vertex_count * sizeof(vertex_t)), (void**)&vtx_dst, D3DLOCK_DISCARD) < 0)
+	if (vertex_buffer->Lock(0, (UINT)(compiled_draw_data.total_vertex_count * sizeof(vertex_t)), (void**)&vtx_dst, D3DLOCK_DISCARD) < 0)
 		throw std::runtime_error{ "vertex_buffer->Lock error" };
-	if (index_buffer->Lock(0, (UINT)(draw_data.total_index_count * sizeof(std::uint32_t)), (void**)&idx_dst, D3DLOCK_DISCARD) < 0)
+	if (index_buffer->Lock(0, (UINT)(compiled_draw_data.total_index_count * sizeof(std::uint32_t)), (void**)&idx_dst, D3DLOCK_DISCARD) < 0)
 		throw std::runtime_error{ "index_buffer->Lock error" };
 
 	vertex_buffer->Unlock();
@@ -57,6 +50,19 @@ void lucid_engine::renderer::render_draw_data(compiled_draw_data_t draw_data) {
 	lucid_engine::graphics::get_instance().direct_3d_device->SetIndices(index_buffer);
 	lucid_engine::graphics::get_instance().direct_3d_device->SetVertexDeclaration(vertex_declaration);
 	lucid_engine::graphics::get_instance().direct_3d_device->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1);
+
+	//Figure out how to build geometry buffer here.
+
+
+	lucid_engine::graphics::get_instance().direct_3d_device->SetTransform(D3DTS_WORLD, &last_world);
+	lucid_engine::graphics::get_instance().direct_3d_device->SetTransform(D3DTS_VIEW, &last_view);
+	lucid_engine::graphics::get_instance().direct_3d_device->SetTransform(D3DTS_PROJECTION, &last_projection);
+
+	d3d9_state_block->Apply();
+	d3d9_state_block->Release();
+
+	compiled_draw_data.vertices.clear();
+	draw_data.clear();
 }
 
 void lucid_engine::renderer::line(vec2_t from, vec2_t to, color_t color) {
@@ -87,6 +93,6 @@ void lucid_engine::renderer::filled_rectangle(vec2_t pos, vec2_t size, color_t c
 	vertices.emplace_back(vertex_t(pos.x + size.x, pos.y, 0.f, 1.f, color.translate()));
 	vertices.emplace_back(vertex_t(pos.x + size.x, pos.y + size.y, 0.f, 1.f, color.translate()));
 	vertices.emplace_back(vertex_t(pos.x, pos.y + size.y, 0.f, 1.f, color.translate()));
-
+	
 	lucid_engine::graphics::get_instance().direct_3d_device->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, vertices.size() - 1, vertices.data(), sizeof(vertex_t));
 }
--- lucid/lucid/renderer/renderer.h
+++ lucid/lucid/renderer/renderer.h
@@ -5,14 +5,14 @@
 namespace lucid_engine {
 	class renderer : public singleton<renderer> {
 	private:
+		compiled_draw_data_t compiled_draw_data;
 		std::vector<draw_data_t> draw_data;
 		IDirect3DVertexDeclaration9* vertex_declaration{ };
 		IDirect3DVertexBuffer9* vertex_buffer{ };
 		IDirect3DIndexBuffer9* index_buffer{ };
 
 	public:
-		compiled_draw_data_t get_draw_data();
-		void render_draw_data(compiled_draw_data_t draw_data);
+		void render_draw_data();
 		void line(vec2_t from, vec2_t to, color_t color);
 		void rectangle(vec2_t pos, vec2_t size, color_t color);
 		void filled_rectangle(vec2_t pos, vec2_t size, color_t color);
