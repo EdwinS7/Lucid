5139ba4e5b3af13f0ff50daad5c761011b9a5369
Connor Smith <88218388+EdwinS7@users.noreply.github.com>
2023-04-30 10:11:09 -0400

    fix crash & fixed default draw list clip

--- lucid/lucid/framework/elements/combo.cpp
+++ lucid/lucid/framework/elements/combo.cpp
@@ -11,7 +11,7 @@ void lucid_engine::ui::combo_box(const char* title, int* value, std::vector<cons
 	if (g_input.get()->mouse_hovering_rect(m_elements_pos, combo_size)) {
 		// check if mouse1 is pressed and change the state of the combo box.
 		if (g_input.get()->is_key_pressed(VK_LBUTTON))
-			m_combo_info.at(m_combo_id).in_use = !m_combo_info.at(m_combo_id).in_use;
+			m_combo_info[m_combo_id].in_use = !m_combo_info[m_combo_id].in_use;
 
 		// set this so we cannot drag our menu.
 		m_hovering_element = true;
@@ -19,8 +19,8 @@ void lucid_engine::ui::combo_box(const char* title, int* value, std::vector<cons
 
 	// check if combo box is even visible and render if visible.
 	if (g_input.get()->rect_clipping_rect(m_elements_pos, combo_size, m_window_pos[m_window_id] + m_group_pos[m_group_id] + vec2_t(0, 26), m_group_size[m_group_id] - vec2_t(0, 26))) {
-		g_renderer.get()->filled_rounded_rectangle(m_elements_pos, combo_size, m_style->m_element_inactive, m_style->m_element_rounding, m_combo_info.at(m_combo_id).in_use ? corner_top : corner_all);
-		g_renderer.get()->rounded_rectangle(m_elements_pos, combo_size, m_style->m_element_outline, m_style->m_element_rounding, m_combo_info.at(m_combo_id).in_use ? corner_top : corner_all);
+		g_renderer.get()->filled_rounded_rectangle(m_elements_pos, combo_size, m_style->m_element_inactive, m_style->m_element_rounding, m_combo_info[m_combo_id].in_use ? corner_top : corner_all);
+		g_renderer.get()->rounded_rectangle(m_elements_pos, combo_size, m_style->m_element_outline, m_style->m_element_rounding, m_combo_info[m_combo_id].in_use ? corner_top : corner_all);
 
 		// render our element.
 		g_renderer.get()->text(g_renderer.get()->m_defualt_font, title, m_elements_pos + vec2_t(3, 1), m_style->m_text_active);
@@ -29,7 +29,7 @@ void lucid_engine::ui::combo_box(const char* title, int* value, std::vector<cons
 		g_renderer.get()->set_draw_list(foreground_draw_list);
 
 		// if the combo is opened then lopp through elements and handle them.
-		if (m_combo_info.at(m_combo_id).in_use) {
+		if (m_combo_info[m_combo_id].in_use) {
 			// render background & outline.
 			g_renderer.get()->filled_rounded_rectangle(m_elements_pos + vec2_t(0, combo_size.y), vec2_t(combo_size.x, combo_size.y * options.size()), m_style->m_element_inactive, m_style->m_element_rounding, corner_bottom);
 			g_renderer.get()->rounded_rectangle(m_elements_pos + vec2_t(0, combo_size.y), vec2_t(combo_size.x, combo_size.y * options.size()), m_style->m_element_outline, m_style->m_element_rounding, corner_bottom);
@@ -42,7 +42,7 @@ void lucid_engine::ui::combo_box(const char* title, int* value, std::vector<cons
 					// check if mouse1 is pressed and change the value of the combo box.
 					if (g_input.get()->is_key_pressed(VK_LBUTTON)) {
 						//close combo box menu.
-						m_combo_info.at(m_combo_id).in_use = false;
+						m_combo_info[m_combo_id].in_use = false;
 
 						// change value of current selected object.
 						*value = i;
